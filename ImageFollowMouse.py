83c306ba36ddbd4ce4b8b54af265d86774776672
Ethan <81988057+EthanCantCode@users.noreply.github.com>
2025-04-17 08:43:59 -0500

    Add files via upload

---
+++ ImageFollowMouse.py
@@ -0,0 +1,33 @@
+import pygame
+from pyrect import WIDTH, HEIGHT
+import keyboard
+
+pygame.init()
+
+WIDTH = 500
+HEIGHT = 500
+IWIDTH = 50
+IHEIGHT = 50
+fps = 60
+timer = pygame.time.Clock()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+picture = pygame.transform.scale(pygame.image.load('/Users/ethantrombley/Desktop/Images/IMG_7853.jpg'), (IWIDTH, IHEIGHT))
+
+run = True
+while run:
+    screen.fill('white')
+    timer.tick(fps)
+    mouse_position = pygame.mouse.get_pos()
+    # pygame.draw.circle(screen,'purple', mouse_position, 10)
+
+    # Adjust the position so the image centers on the mouse
+    pic_rect = picture.get_rect(center=mouse_position)
+    screen.blit(picture, pic_rect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
